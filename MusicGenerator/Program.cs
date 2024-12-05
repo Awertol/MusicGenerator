4dca89a18e0983c20ed33ad218b43ef3fcb30cb7
Awertol <michal.helgert@seznam.cz>
2024-12-06 00:20:23 +0100

    Odstraněna zbytečná podmínka, upraven komentář

--- MusicGenerator/Program.cs
+++ MusicGenerator/Program.cs
@@ -4,10 +4,6 @@ while(1+1 == 2) //podmínka co stále platí, nikdy neskončí
     int nahodnaFrekvence = generator.Next(37,2200); //frekvence zvuku mezi 37 a 2200 - nad to je to fuj:(
     int nahodnaDelka = generator.Next(100, 700); //délka mezi 100 a 700 (jsou to milisekundy)
     Console.Beep(nahodnaFrekvence, nahodnaDelka); //zapípá vygenerovanou frekvencí na vygenerovaný počet milisekund
-    Console.WriteLine(nahodnaFrekvence + "Hz " + nahodnaDelka + "ms"); //vypíše proměnné (+ text)
-    if(nahodnaFrekvence == 100)
-    { 
-        Console.WriteLine("- TRALALALALALALA");
-    }
+    Console.WriteLine(nahodnaFrekvence + "Hz " + nahodnaDelka + "ms"); //vypíše proměnné (+ text s jednotkami)
 }
 
